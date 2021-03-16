2123fd0c76891bd932e036f27bbdf0761a672d74
jiselectric9320 <jiselectric9320@gmail.com>
2021-03-16 22:09:25 +0900

    first commit

---
+++ jh_react/start/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Home from './routers/Home';
+import List from './routers/List';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <ul>
+                <li>
+                    <Link to="/home"> To Home </Link>
+                </li>
+                <li>
+                    <Link to="/list"> To List </Link>
+                </li>
+            </ul>
+            <Route path="/home">
+                <Home />
+            </Route>
+            <Route path="/list">
+                <List />
+            </Route>
+        </BrowserRouter>
+    );
+};
+
+export default App;
---
+++ jh_react/start/src/routers/List.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function list() {
+    return (
+        <h1> List Component </h1>
+    );
+};
+
+export default list;
