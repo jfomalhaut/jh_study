89feb095fcef8a3fdb35695812b9cb5347bffe80
이준 <dlwnssla@jfomalhaut.com>
2021-04-01 14:54:19 +0900

    dd

--- jh_react/start/src/index.js
+++ jh_react/start/src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from './App';
-<<<<<<< HEAD
 import Addr from './routers/Addr';
 
 ReactDom.render(<Addr />, document.getElementById('root'));
-=======
-import Game from './routers/Game';
-
-ReactDom.render(<Game />, document.getElementById('root'));
->>>>>>> 4b569f46ff94aaa0dede1900db9fe16a6f660d02
--- lj_react/start2/src/routers/Addr.js
+++ lj_react/start2/src/routers/Addr.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 
 const API_KEY = 'devU01TX0FVVEgyMDIxMDMxMjExMDIwNDExMDkwNzM=';
@@ -6,16 +6,18 @@ const URL = 'https://www.juso.go.kr/addrlink/addrLinkApi.do';
 
 function Addr() {
 	const [keyword, setKeyword] = useState('');
+	const [keyword2, setKeyword2] = useState('');
 	const [list, setList] = useState([]);
 	const [total, setTotal] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
 
 	const onSearch = (ev) => {
 		ev.preventDefault();
-		search();
+		setKeyword2(keyword);
 	};
 
 	const search = () => {
-		Axios.get(`${URL}?confmKey=${API_KEY}&currentPage=1&CountPerPage=10&resultType=json&keyword=${keyword}`).then(res => {
+		Axios.get(`${URL}?confmKey=${API_KEY}&currentPage=${currentPage}&CountPerPage=10&resultType=json&keyword=${keyword2}`).then(res => {
 			const { data: { results: { common: { totalCount }, juso } } } = res;
 			setTotal(totalCount || 0);
 			setList(juso || []);
@@ -23,6 +25,30 @@ function Addr() {
 			console.log(error);
 		});
 	};
+
+	const next = () => {
+		setCurrentPage(prev => prev + 1);
+	};
+
+	const prev = () => {
+		setCurrentPage(prev => prev - 1);
+	};
+
+	useEffect(() => {
+		if (keyword2 !== '') {
+			if (currentPage !== 1) {
+				setCurrentPage(1);
+				return;
+			}
+			search();
+		}
+	}, [keyword2]);
+
+	useEffect(() => {
+		if (keyword2 !== '') {
+			search();
+		}
+	}, [currentPage]);
 
 	return (
 		<div>
@@ -32,11 +58,16 @@ function Addr() {
 				<button>검색</button>
 			</form>
 			<h1>검색결과: 총 {total}개의 주소가 검색되었습니다</h1>
+			<h1>현재 페이지: {currentPage}</h1>
 			<ul>
 				{list.map((item, idx) => (
 					<li key={`ADDRESS${idx}`}>[{item.zipNo}] {item.roadAddrPart1}</li>
 				))}
 			</ul>
+			{currentPage > 1 && (
+				<button onClick={prev}>이전</button>
+			)}
+			<button onClick={next}>다음</button>
 		</div>
 	);
 }
